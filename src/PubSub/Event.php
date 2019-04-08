0aa831da2a785cc5d6dd2aea41038da8b91ae373
Brad Kent <bkfake-github@yahoo.com>
2019-04-07 19:43:45 -0500

    Update copyright date

--- src/PubSub/Event.php
+++ src/PubSub/Event.php
@@ -5,7 +5,7 @@
  * @package   bdk\PubSub
  * @author    Brad Kent <bkfake-github@yahoo.com>
  * @license   http://opensource.org/licenses/MIT MIT
- * @copyright 2014-2018 Brad Kent
+ * @copyright 2014-2019 Brad Kent
  * @version   v2.3
  * @link      http://www.github.com/bkdotcom/PubSub
  */

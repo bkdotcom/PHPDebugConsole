96cc818f4e7e4d86c45797a38b17338823ef1b05
Brad Kent <bkfake-github@yahoo.com>
2020-02-04 10:06:14 -0600

    use post-update-cmd vs post-install-cmd

--- src/Debug/ComposerScripts.php
+++ src/Debug/ComposerScripts.php
@@ -15,11 +15,14 @@ class ComposerScripts
     /**
      * Require slevomat/coding-standard if dev mode & PHP >= 7.1
      *
+     * Ran after the update command has been executed,
+     *   or after the install command has been executed without a lock file present.
+     *
      * @param Event $event Composer event instance
      *
      * @return void
      */
-    public static function postInstall(Event $event)
+    public static function postUpdate(Event $event)
     {
         if ($event->isDevMode() && \version_compare(PHP_VERSION, '7.1', '>=')) {
             \exec('composer require slevomat/coding-standard --dev');

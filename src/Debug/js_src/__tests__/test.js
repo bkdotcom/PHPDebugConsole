c5f6317869d6af2e5c8674c5969e7811fb4471e1
Brad Kent <bkfake-github@yahoo.com>
2020-04-27 10:50:20 -0500

    javascript test:  add beforeAll() method

--- src/Debug/js_src/__tests__/test.js
+++ src/Debug/js_src/__tests__/test.js
@@ -1,4 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+
 describe('my first test', () => {
+
+  var html
+
+  beforeAll(() => {
+    html = fs.readFileSync(path.resolve(__dirname, './test.html')).toString()
+    document.body.innerHTML = html
+  });
+
   test('test passes', () => {
     expect(true).toBeTruthy()
   })

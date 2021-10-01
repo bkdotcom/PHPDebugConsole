25053d7411459951c43ab9bcee0777bf00516b53
Brad Kent <bkfake-github@yahoo.com>
2021-09-30 23:39:09 -0500

    fix php 5.x error

--- src/Debug/Utility/Utility.php
+++ src/Debug/Utility/Utility.php
@@ -25,49 +25,6 @@ use SqlFormatter;
 class Utility
 {
 
-    const HTTP_PHRASES = array(
-        '100' => 'Continue',
-        '101' => 'Switching Protocols',
-        '200' => 'OK',
-        '201' => 'Created',
-        '202' => 'Accepted',
-        '203' => 'Non-Authoritative Information',
-        '204' => 'No Content',
-        '205' => 'Reset Content',
-        '206' => 'Partial Content',
-        '300' => 'Multiple Choices',
-        '301' => 'Moved Permanently',
-        '302' => 'Moved Temporarily',
-        '303' => 'See Other',
-        '304' => 'Not Modified',
-        '305' => 'Use Proxy',
-        '307' => 'Temporary Redirect',
-        '400' => 'Bad Request',
-        '401' => 'Unauthorized',
-        '402' => 'Payment Required',
-        '403' => 'Forbidden',
-        '404' => 'Not Found',
-        '405' => 'Method Not Allowed',
-        '406' => 'Not Acceptable',
-        '407' => 'Proxy Authentication Required',
-        '408' => 'Request Time-out',
-        '409' => 'Conflict',
-        '410' => 'Gone',
-        '411' => 'Length Required',
-        '412' => 'Precondition Failed',
-        '413' => 'Request Entity Too Large',
-        '414' => 'Request-URI Too Large',
-        '415' => 'Unsupported Media Type',
-        '416' => 'Range Not Satisfiable',
-        '417' => 'Expectation Failed',
-        '426' => 'Upgrade Required',
-        '500' => 'Internal Server Error',
-        '501' => 'Not Implemented',
-        '502' => 'Bad Gateway',
-        '503' => 'Service Unavailable',
-        '504' => 'Gateway Time-out',
-        '505' => 'HTTP Version not supported',
-    );
     const IS_CALLABLE_ARRAY_ONLY = 1;
     const IS_CALLABLE_OBJ_ONLY = 2;
     const IS_CALLABLE_STRICT = 3;
@@ -309,9 +266,52 @@ class Utility
      */
     public static function httpStatusPhrase($statusCode)
     {
+        $phrases = array(
+            '100' => 'Continue',
+            '101' => 'Switching Protocols',
+            '200' => 'OK',
+            '201' => 'Created',
+            '202' => 'Accepted',
+            '203' => 'Non-Authoritative Information',
+            '204' => 'No Content',
+            '205' => 'Reset Content',
+            '206' => 'Partial Content',
+            '300' => 'Multiple Choices',
+            '301' => 'Moved Permanently',
+            '302' => 'Moved Temporarily',
+            '303' => 'See Other',
+            '304' => 'Not Modified',
+            '305' => 'Use Proxy',
+            '307' => 'Temporary Redirect',
+            '400' => 'Bad Request',
+            '401' => 'Unauthorized',
+            '402' => 'Payment Required',
+            '403' => 'Forbidden',
+            '404' => 'Not Found',
+            '405' => 'Method Not Allowed',
+            '406' => 'Not Acceptable',
+            '407' => 'Proxy Authentication Required',
+            '408' => 'Request Time-out',
+            '409' => 'Conflict',
+            '410' => 'Gone',
+            '411' => 'Length Required',
+            '412' => 'Precondition Failed',
+            '413' => 'Request Entity Too Large',
+            '414' => 'Request-URI Too Large',
+            '415' => 'Unsupported Media Type',
+            '416' => 'Range Not Satisfiable',
+            '417' => 'Expectation Failed',
+            '426' => 'Upgrade Required',
+            '500' => 'Internal Server Error',
+            '501' => 'Not Implemented',
+            '502' => 'Bad Gateway',
+            '503' => 'Service Unavailable',
+            '504' => 'Gateway Time-out',
+            '505' => 'HTTP Version not supported',
+        );
         $statusCode = (int) $statusCode;
-        return isset(self::HTTP_PHRASES[$statusCode])
-            ? self::HTTP_PHRASES[$statusCode]
+        return isset($phrases[$statusCode])
+            ? $phrases[$statusCode]
             : '';
     }
 

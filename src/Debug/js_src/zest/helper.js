926ed540ed46653176d5caff86f3ce35a4eca9d0
Brad Kent <bkfake-github@yahoo.com>
2026-01-30 11:12:18 -0600

    Zest:  typeof object / null bug

--- src/Debug/js_src/zest/helper.js
+++ src/Debug/js_src/zest/helper.js
@@ -72,7 +72,7 @@ export const argsToElements = function (args, acceptSelector, el, index) {
       elements.push(arg)
     } else if (arg instanceof NodeList) {
       elements.push(...arg)
-    } else if (typeof arg === 'object' && typeof arg[Symbol.iterator] === 'function') {
+    } else if (typeof arg === 'object' && arg !== null && typeof arg[Symbol.iterator] === 'function') {
       args.unshift(...arg)
     } else if (typeof arg === 'function') {
       args.unshift(arg.call(el, el, index))
